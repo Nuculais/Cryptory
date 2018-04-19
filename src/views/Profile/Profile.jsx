f04cec15c0918570638189ea95c755e0f7357844
John Castronuovo <johncas@kth.se>
2018-04-19 09:40:24 +0200

    feat(currencyAPI): load ticker on profile

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -6,74 +6,113 @@ import {render} from "react-dom";
 
 class Profile extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       title: 'Cryptory Profile',
-      status: 'INITIAL',
-      user: ''
+      status_prof: 'INITIAL',
+      status_ticker: 'INITIAL',
+      user: '',
+      currencies: ''
     };
-  }
 
-  loadData() {
-    fetch(`api/user/${user.data}`).then(response => {
-      if (response.ok) {
-        response.json().then(data => {
-          this.setState({
-            status: 'LOADED',
-            user: data
+    // TODO: pull user preferences into url call
+    const CUR_API = `https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR`
+    loadCurrencyData = () => {
+      fetch(
+        CUR_API)
+        .then(response => {
+          if (response.ok) {
+            response.json().then(data => {
+              this.setState({
+                status: 'LOADED',
+                currency: data
+              })
+            })
+          } else {
+            response.json().then(error => {
+              alert("Failed to fetch issues:" + error.message)
+            });
+          }
+        }).catch(err => {
+        alert("Error in fetching currency data from server:", err);
+      });
+    };
+
+    loadData = () => {
+      fetch(`api/user/${user.data}`).then(response => {
+        if (response.ok) {
+          response.json().then(data => {
+            this.setState({
+              status: 'LOADED',
+              user: data
+            });
           });
-        });
-      } else {
-        response.json().then(error => {
-          alert("Failed to fetch issues:" + error.message)
-        });
-      }
-    }).catch(err => {
-      alert("Error in fetching data from server:", err);
-    });
-  }
+        } else {
+          response.json().then(error => {
+            alert("Failed to fetch issues:" + error.message)
+          });
+        }
+      }).catch(err => {
+        alert("Error in fetching user data from server:", err);
+      });
+    };
 
-  componentDidMount() {
-    this.loadData()
-  }
-
-  render() {
-    console.log('home props', this.props)
-    let profile, graphs = ''
-    switch (this.state.status) {
-      case 'INITIAL':
-        profile = <em>Loading...</em>
-        graphs = <em>Loading...</em>
-        break;
-      case 'LOADED':
-        profile =
-          <div>
-            <h3>Welcome, {this.state.user.data.name}!</h3>
-            <img src={this.state.user.data.avatar}/>
-          </div>
-        graphs = this.state.user.data.following.map((currency) =>
-          <li key={currency.id}>{currency}</li>
-        );
-        break;
-    }
+    componentDidMount()
     {
-      console.log(this.state)
+      this.loadData()
+      this.loadCurrencyData()
     }
 
-    return (
-      <div>
-        <h1>{this.state.title}</h1>
-        {profile}
-        <h2>Your Currencies:</h2>
-        {graphs}
-        <p><a href="/logout">log out</a></p>
-      </div>
-    );
+    render()
+    {
+      console.log('home props', this.props)
+      let profile, graphs, ticker = ''
+      switch (this.state.status_prof) {
+        case 'INITIAL':
+          profile = <em>Loading...</em>
+          graph = <em>Loading...</em>
+          break;
+        case 'LOADED':
+          profile =
+            <div>
+              <h3>Welcome, {this.state.user.data.name}!</h3>
+              <img src={this.state.user.data.avatar}/>
+            </div>
+          graphs = this.state.user.data.following.map((currency) =>
+            <li key={currency.id}>{currency}</li>
+          );
+          break;
+      }
+      switch (this.state.status_ticker) {
+        case 'INITIAL':
+          ticker = <em>Loading..</em>
+          break;
+        case 'LOADED':
+          ticker = <div>{this.state.currencies}</div>
+          break;
+      }
+      {
+        console.log(this.state)
+      }
+
+      return (
+        <div>
+          <h1>{this.state.title}</h1>
+          {profile}
+          <h2>Your Currencies:</h2>
+          {graphs}
+          <br/>
+          {ticker}
+          <br/>
+          <p><a href="/logout">log out</a></p>
+        </div>
+      );
+    }
   }
 }
 
 render(
-  <Profile/>
-  ,
+  <Profile/>,
   document.getElementById('profile'));
+
 module.hot.accept();
